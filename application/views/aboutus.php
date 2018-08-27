3f13a9ea69c0a634eafdca0807d520c28da3f583
Sanket <sdsanketdhumal2104@gmail.com>
2018-08-28 01:46:30 +0530

    completed about us

---
+++ application/views/aboutus.php
@@ -0,0 +1,16 @@
+<div class="clearfix wow fadeInUp animated" style="visibility: visible; animation-name: fadeInUp; text-align: justify;padding-right: 20px;padding-left:20px">
+
+                        <h3>About Us</h3>
+<p>E-Disposer is an online platform for buyers as well as sellers for e-waste where different users and companies can buy and sell any type of electronic waste it has. Through this app various individual or companies would be able dispose its e-waste properly so that it doesn’t affect the environment and is disposed in a correct way.</p>
+<p>
+The objectives of this system are as follows.
+<ul>
+	<li>Online platform for buyer and seller of e-waste</li>
+	<li>Proper disposal of electronic devices</li>
+	<li>Online record of all devices brought and sold.</li>
+	<li>Information on various disposal centers.</li>
+	<li>Recycling the equipment using proper channel</li>
+</ul>	
+</p>
+The project will be largely divided under many users who will be interested to buy and sell e-waste for their respective use. The seller will mostly be a firm which will be dealing with its actual processing for disposal after which they would be creating new products or will be recycling it. Also many users and firms may provide information regarding collection of their e-waste details so that interested user can go and buy it online. Also the Municipal Corporation can use it full their purpose regarding the e-waste disposal.
+</p> </div>
